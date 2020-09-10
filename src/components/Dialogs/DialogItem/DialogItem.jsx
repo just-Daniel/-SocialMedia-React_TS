546a3dde8394cf2c0527bc54c415541b780c10f4
Your Name <you@example.com>
2020-09-10 16:39:28 +0300

    add class active

--- src/components/Dialogs/DialogItem/DialogItem.jsx
+++ src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -8,7 +8,7 @@ const DialogItem = (props) => {
   return (
     <div className={s.item}>
       <img src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""/>
-      <NavLink to={ path }>{ props.name }</NavLink>
+      <NavLink to={ path } activeClassName={s.active}>{ props.name }</NavLink>
     </div>
   )
 }
