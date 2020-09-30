cdb0e2036ec1e67f20e996c9014935d3cadfbecb
Your Name <you@example.com>
2020-09-30 23:21:58 +0300

    encapsulation contextAPI

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import StoreContext, { Provider } from './StoreContext';
+import { Provider } from './StoreContext';
 
 let rerenderEntireTree = (state) => {
   ReactDOM.render(
